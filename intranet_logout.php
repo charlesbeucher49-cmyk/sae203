05d5d025642cc1878ae6a57ae6dbb79bf1b3d2fe
Charles Beucher <charlesbeucher@dn054049.etu.univ-rennes1.fr>
2026-06-01 16:44:35 +0200

    feat(auth): ajout et correction du portail de connexion et de la page d'accueil

--- intranet_logout.php
+++ intranet_logout.php
@@ -1,5 +1,6 @@
-<?php session_start();
-session_unset();
+<?php
+session_start();
 session_destroy();
-header("Location:portail_connexion.php");
+header('Location: intranet_login.php');
+exit();
 ?>
